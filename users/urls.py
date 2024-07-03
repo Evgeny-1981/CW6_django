de6bc744a471346e218176bc90032ae314f9fc26
evgeny <eaegorov@gmail.com>
2024-07-03 16:11:24 +0500

    курсовая работа 6 django

--- users/urls.py
+++ users/urls.py
@@ -13,7 +13,7 @@ from django.contrib.auth.views import (
 app_name = UsersConfig.name
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(template_name='catalog/home.html'), name='logout'),
+    # path('logout/', LogoutView.as_view(template_name='catalog/home.html'), name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
     path('profile/', ProfileView.as_view(), name='profile'),
     path("email-confirm/<str:token>/", email_verification, name='email-confirm'),
