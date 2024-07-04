08f3702efd718e3105be140f5416e1063ac3ee7f
evgeny <eaegorov@gmail.com>
2024-07-04 15:01:59 +0500

    курсовая работа 6 django

--- mailings/models.py
+++ mailings/models.py
@@ -1,80 +1,82 @@
 from django.db import models
 
 from users.models import User
+from django.utils import timezone
 
 NULLABLE = {"blank": True, "null": True}
+Frequency_of_mailing = [("Daily", "Ежедневно"), ("Weekly", "Еженедельно"), ("Monthly", "Ежемесячно"), ]
+Mailing_status = [("Completed", "Завершена"), ("Created", "Создана"), ("Launched", "Запущена"), ]
+Permissions = [('View_any_mailing_lists', 'Просматривать любые рассылки'),
+               ('Disable_mailing_lists', 'Отключать рассылки'), ]
 
 
 class Client(models.Model):
-    """Модель для блога"""
-    email = models.EmailField(unique=True, verbose_name='почта')
+    """Модель для клиента"""
+    email = models.EmailField(unique=True, verbose_name='Электронная почта')
     fio = models.CharField(max_length=150, verbose_name='ФИО')
     comment = models.TextField(verbose_name='Комментарий', **NULLABLE)
-    owner = models.ForeignKey(User, verbose_name="Чей клиент", related_name="clients", on_delete=models.SET_NULL,
-                              **NULLABLE,)
+    owner_client = models.ForeignKey(User, verbose_name="Чей клиент", related_name="clients", on_delete=models.SET_NULL,
+                                     **NULLABLE, )
 
     class Meta:
         db_table = "client"
         verbose_name = "Клиент"
-        verbose_name_plural = "Клиента"
-        ordering = ("email",)
+        verbose_name_plural = "Клиенты"
+        ordering = ("email", "owner_client",)
 
     def __str__(self):
         return f"Клиент: {self.fio}, {self.email}"
 
 
-class Mailing(models.Model):
-    """Модель для блога"""
-    title = models.CharField(max_length=120, verbose_name="заголовок", unique=True)
-    slug = models.CharField(max_length=120, verbose_name="слаг", unique=True)
-    content = models.TextField(verbose_name="содержимое")
-    preview = models.ImageField(upload_to="blog/images", verbose_name="превью(изображение)", **NULLABLE)
-    created_at = models.DateField(auto_now_add=True, verbose_name="дата публикации")
-    published = models.BooleanField(verbose_name="признак публикации", default=False)
-    count_views = models.PositiveIntegerField(verbose_name="счетчик просмотров", default=0, **NULLABLE)
-
-    class Meta:
-        db_table = "blog"
-        verbose_name = "блог"
-        verbose_name_plural = "блоги"
-        ordering = ("count_views",)
-
-    def __str__(self):
-        return f"{self.title}, {self.content}"
-
-
 class Message(models.Model):
-    """Модель для блога"""
-    subject = models.CharField(max_length=120, verbose_name="Тема письма",)
-    message = models.TextField(verbose_name="Сообщение",)
-    owner = models.ForeignKey(User, verbose_name="Автор сообщения", **NULLABLE, on_delete=models.SET_NULL,)
-
+    """Модель для сообщений"""
+    subject = models.CharField(max_length=120, verbose_name="Тема письма", )
+    message = models.TextField(verbose_name="Сообщение", )
+    owner_message = models.ForeignKey(User, verbose_name="Автор сообщения", on_delete=models.SET_NULL, **NULLABLE, )
 
     class Meta:
         db_table = "message"
         verbose_name = "Сообщение"
         verbose_name_plural = "Сообщения"
-        ordering = ("subject",)
+        ordering = ("subject", "owner_message",)
 
     def __str__(self):
         return f"Тема сообщения: {self.subject}"
 
 
-class MailingAttempt(models.Model):
-    """Модель для блога"""
-    title = models.CharField(max_length=120, verbose_name="заголовок", unique=True)
-    slug = models.CharField(max_length=120, verbose_name="слаг", unique=True)
-    content = models.TextField(verbose_name="содержимое")
-    preview = models.ImageField(upload_to="blog/images", verbose_name="превью(изображение)", **NULLABLE)
-    created_at = models.DateField(auto_now_add=True, verbose_name="дата публикации")
-    published = models.BooleanField(verbose_name="признак публикации", default=False)
-    count_views = models.PositiveIntegerField(verbose_name="счетчик просмотров", default=0, **NULLABLE)
+class Mailing(models.Model):
+    """Модель для рассылки"""
+    datetime = models.DateTimeField(default=timezone.now, verbose_name="Дата и время первой отправки")
+    owner_mailing = models.ForeignKey(User, verbose_name="Автор рассылки", on_delete=models.SET_NULL, **NULLABLE, )
+    message = models.ForeignKey(Message, verbose_name="Сообщение рассылки", on_delete=models.CASCADE)
+    frequency = models.CharField(max_length=15, choices=Frequency_of_mailing, verbose_name="Периодичность рассылки")
+    status = models.CharField(max_length=10, choices=Mailing_status, default="created",
+                              verbose_name="Статус рассылки", )
+    clients = models.ManyToManyField(Client, verbose_name="Получатели рассылки")
+    is_active = models.BooleanField(default=True, verbose_name="Актуальность рассылки")
 
     class Meta:
-        db_table = "blog"
-        verbose_name = "блог"
-        verbose_name_plural = "блоги"
-        ordering = ("count_views",)
+        db_table = "mailing"
+        verbose_name = "Рассылка"
+        verbose_name_plural = "Рассылки"
+        ordering = ("datetime", "status",)
 
     def __str__(self):
-        return f"{self.title}, {self.content}"
+        return f"Рассылка: {self.pk}. Время: {self.datetime}. Статус: {self.status}"
+
+
+class MailingAttempt(models.Model):
+    """Модель для блога"""
+    datetime = models.DateTimeField(auto_now=True, verbose_name="Дата и время рассылки", )
+    status = models.CharField(max_length=120, verbose_name="Статус рассылки", )
+    answer = models.TextField(verbose_name="Ответ сервера", **NULLABLE, )
+    owner_mailing = models.ForeignKey(User, verbose_name="Владелец рассыылки", on_delete=models.SET_NULL, **NULLABLE)
+
+    class Meta:
+        db_table = "MailingAttempt"
+        verbose_name = "Отчет рассылки"
+        verbose_name_plural = "Отчеты рассылки"
+        ordering = ("status", "answer", "owner_mailing",)
+
+    def __str__(self):
+        return f"{self.datetime}, {self.status}, {self.answer}"
--- users/models.py
+++ users/models.py
@@ -2,15 +2,17 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 NULLABLE = {"blank": True, "null": True}
+Permissions = [('View_the_list_of_service_users', 'Просматривать список пользователей сервиса'),
+               ('Block_users_of_the_service', 'Блокировать пользователей сервиса'), ]
 
 
 class User(AbstractUser):
     username = None
-    email = models.EmailField(unique=True, verbose_name='почта')
-    phone = models.CharField(max_length=30, verbose_name='телефон', **NULLABLE)
-    avatar = models.ImageField(upload_to='users/', verbose_name='автар', **NULLABLE)
-    country = models.CharField(max_length=40, verbose_name='страна', **NULLABLE)
-    token = models.CharField(max_length=100, verbose_name='токен', **NULLABLE)
+    email = models.EmailField(unique=True, verbose_name='Электронная почта')
+    phone = models.CharField(max_length=30, verbose_name='Контактный телефон', **NULLABLE)
+    avatar = models.ImageField(upload_to='users/', verbose_name='Автар', **NULLABLE)
+    country = models.CharField(max_length=40, verbose_name='Страна', **NULLABLE)
+    token = models.CharField(max_length=100, verbose_name='Токен', **NULLABLE)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
@@ -20,6 +22,7 @@ class User(AbstractUser):
 
     class Meta:
         db_table = "user"
-        verbose_name = "пользователь"
-        verbose_name_plural = "пользователи"
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
         ordering = ("email",)
+        permissions = Permissions
