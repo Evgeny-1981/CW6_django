2a94375412a7f4633a354c752a5748cb93aded80
evgeny <eaegorov@gmail.com>
2024-07-08 16:39:54 +0500

    курсовая работа 6 django

--- mailings/urls.py
+++ mailings/urls.py
@@ -2,19 +2,31 @@ from django.urls import path
 from django.views.decorators.cache import cache_page
 
 from mailings.apps import MailingsConfig
-from mailings.views import MailingListView, MailingDetailView, MailingCreateView
+from mailings.views import MailingListView, MailingDetailView, MailingCreateView, MailingUpdateView, MailingDeleteView, \
+    ContactsView, ClientListView, ClientDetailView, ClientCreateView, ClientUpdateView, ClientDeleteView, \
+    MessageListView, MessageDetailView, MessageCreateView, MessageUpdateView, MessageDeleteView
 
 app_name = MailingsConfig.name
 
 urlpatterns = [
     # path('categoryes/', CategoryListView.as_view(), name='categoryes_list'),
     # path('category/create', CategoryCreateView.as_view(), name='category_create'),
-    path('', MailingListView.as_view(), name='mailing_list'),
+    path('', MailingListView.as_view(), name='mailings_list'),
     path('mailings/<int:pk>/', MailingDetailView.as_view(), name='mailing_info'),
     path('mailings/create', MailingCreateView.as_view(), name='mailing_create'),
-    # path('mailings/<str:slug>/update/', ProductUpdateView.as_view(), name='product_update'),
-    # path('mailings/<str:slug>/delete/', ProductDeleteView.as_view(), name='product_delete'),
-    # path('contacts/', cache_page(60)(ContactsView.as_view()), name='contacts'),
+    path('mailings/<int:pk>/update/', MailingUpdateView.as_view(), name='mailing_update'),
+    path('mailings/<int:pk>/delete/', MailingDeleteView.as_view(), name='mailing_delete'),
+    path('clients/', ClientListView.as_view(), name='clients_list'),
+    path('clients/<int:pk>/', ClientDetailView.as_view(), name='client_info'),
+    path('clients/create', ClientCreateView.as_view(), name='client_create'),
+    path('clients/<int:pk>/update/', ClientUpdateView.as_view(), name='client_update'),
+    path('clients/<int:pk>/delete/', ClientDeleteView.as_view(), name='client_delete'),
+    path('messages/', MessageListView.as_view(), name='messages_list'),
+    path('messages/<int:pk>/', MessageDetailView.as_view(), name='message_info'),
+    path('messages/create', MessageCreateView.as_view(), name='message_create'),
+    path('messages/<int:pk>/update/', MessageUpdateView.as_view(), name='message_update'),
+    path('messages/<int:pk>/delete/', MessageDeleteView.as_view(), name='message_delete'),
+    path('contacts/', cache_page(60)(ContactsView.as_view()), name='contacts'),
     # path('blogs/', BlogListView.as_view(), name='blog_list'),
     # path('blogs/<slug:slug>/', BlogDetailView.as_view(), name='blog_info'),
     # path('blogs/create', BlogCreateView.as_view(), name='blog_create'),
--- mailings/views.py
+++ mailings/views.py
@@ -6,7 +6,7 @@ from django.urls import reverse_lazy, reverse
 from django.views.generic import ListView, DetailView, TemplateView, CreateView, UpdateView, DeleteView
 from pytils.translit import slugify
 
-from mailings.forms import MailingForm, MailingModeratorForm
+from mailings.forms import MailingForm, ClientForm, MessageForm, MailingModeratorForm
 from mailings.models import Client, Message, Mailing, MailingAttempt
 
 
@@ -23,9 +23,9 @@ class MailingListView(ListView):
     #     if user.has_perm('mailings.View_any_mailing_lists'):
     #         queryset = queryset.all()
     #         return queryset
-        # else:
-        #     queryset = queryset.filter(published=True)
-        #     return queryset
+    # else:
+    #     queryset = queryset.filter(published=True)
+    #     return queryset
 
     # def get_context_data(self, *args, **kwargs):
     #     """Метод для получения версии продукта и вывода только активной версии"""
@@ -46,4 +46,119 @@ class MailingCreateView(LoginRequiredMixin, CreateView):
     """Контроллер для создания рассылки"""
     model = Mailing
     form_class = MailingForm
-    success_url = reverse_lazy('mailings:mailing_list')
+    success_url = reverse_lazy('mailings:mailings_list')
+
+
+class MailingUpdateView(LoginRequiredMixin, UpdateView):
+    model = Mailing
+    form_class = MailingForm
+    success_url = reverse_lazy("mailings:mailings_list")
+
+
+class MailingDeleteView(LoginRequiredMixin, DeleteView):
+    model = Mailing
+    success_url = reverse_lazy("mailings:mailings_list")
+
+class ClientListView(ListView):
+    """Контроллер отображения страницы с клиентами"""
+    model = Client
+
+    # def get_queryset(self, *args, **kwargs):
+    #     queryset = super().get_queryset()
+    #     user = self.request.user
+    #     if user.has_perm('mailings.View_any_mailing_lists'):
+    #         queryset = queryset.all()
+    #         return queryset
+    # else:
+    #     queryset = queryset.filter(published=True)
+    #     return queryset
+
+    # def get_context_data(self, *args, **kwargs):
+    #     """Метод для получения версии продукта и вывода только активной версии"""
+    #     context = super().get_context_data(*args, **kwargs)
+    #     products = self.get_queryset()
+    #     for product in products:
+    #         product.version_name = product.version.filter(current_version=True).first()
+    #     context["object_list"] = products
+    #     return context
+
+
+class ClientDetailView(DetailView):
+    """Контроллер для просмотра клиента"""
+    model = Client
+
+
+class ClientCreateView(LoginRequiredMixin, CreateView):
+    """Контроллер для создания клиента"""
+    model = Client
+    form_class = ClientForm
+    success_url = reverse_lazy('mailings:clients_list')
+
+
+class ClientUpdateView(LoginRequiredMixin, UpdateView):
+    model = Client
+    form_class = ClientForm
+    success_url = reverse_lazy('mailings:clients_list')
+
+
+class ClientDeleteView(LoginRequiredMixin, DeleteView):
+    model = Client
+    success_url = reverse_lazy('mailings:clients_list')
+
+
+class MessageListView(ListView):
+    """Контроллер отображения страницы с сообщениями"""
+    model = Message
+
+    # def get_queryset(self, *args, **kwargs):
+    #     queryset = super().get_queryset()
+    #     user = self.request.user
+    #     if user.has_perm('mailings.View_any_mailing_lists'):
+    #         queryset = queryset.all()
+    #         return queryset
+    # else:
+    #     queryset = queryset.filter(published=True)
+    #     return queryset
+
+    # def get_context_data(self, *args, **kwargs):
+    #     """Метод для получения версии продукта и вывода только активной версии"""
+    #     context = super().get_context_data(*args, **kwargs)
+    #     products = self.get_queryset()
+    #     for product in products:
+    #         product.version_name = product.version.filter(current_version=True).first()
+    #     context["object_list"] = products
+    #     return context
+
+
+class MessageDetailView(DetailView):
+    """Контроллер для просмотра сообщения"""
+    model = Message
+
+
+class MessageCreateView(LoginRequiredMixin, CreateView):
+    """Контроллер для создания сообщения"""
+    model = Message
+    form_class = MessageForm
+    success_url = reverse_lazy('mailings:messages_list')
+
+
+class MessageUpdateView(LoginRequiredMixin, UpdateView):
+    model = Message
+    form_class = MessageForm
+    success_url = reverse_lazy('mailings:messages_list')
+
+
+class MessageDeleteView(LoginRequiredMixin, DeleteView):
+    model = Message
+    success_url = reverse_lazy('mailings:messages_list')
+
+
+class ContactsView(TemplateView):
+    template_name = 'mailings/contacts.html'
+
+    def post(self, request):
+        name = request.POST.get('name', '')
+        phone = request.POST.get('phone', '')
+        message = request.POST.get('message', '')
+        print(f'Имя:{name}, тел.:{phone}, сообщение: {message}')
+        return HttpResponseRedirect(self.request.path)
